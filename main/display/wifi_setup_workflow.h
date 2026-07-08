dc066b1302a3d67e617a9c6f5390d6ac49fa2bb2
Remiehneppo <bao.tran080898@gmail.com>
2026-07-08 08:42:51 +0700

    Revert "fix(wifi): cache setup scan results"
    This reverts commit 2a171a4ba30033794029d46d887caf5797f10b50.

--- main/display/wifi_setup_workflow.h
+++ main/display/wifi_setup_workflow.h
@@ -1,15 +1,24 @@
 #ifndef WIFI_SETUP_WORKFLOW_H
 #define WIFI_SETUP_WORKFLOW_H
 
+#include <esp_event.h>
 #include <esp_err.h>
 #include <string>
 #include <vector>
 
 class WifiSetupWorkflow {
 public:
-    esp_err_t ScanSsids(std::vector<std::string>& ssids);
+    ~WifiSetupWorkflow();
+
+    esp_err_t RegisterScanDoneHandler(esp_event_handler_t handler, void* arg);
+    void UnregisterScanDoneHandler();
+    esp_err_t StartScan();
     bool GetScannedSsids(std::vector<std::string>& ssids);
     void SaveCredentialsAndReconnect(const char* ssid, const char* password);
+
+private:
+    bool scan_handler_registered_ = false;
+    esp_event_handler_t scan_done_handler_ = nullptr;
 };
 
 #endif // WIFI_SETUP_WORKFLOW_H
